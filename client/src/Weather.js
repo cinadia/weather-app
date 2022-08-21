e0ef51057c4f92b5a4da72348ea9f60ff6125217
cinadia <73308097+cinadia@users.noreply.github.com>
2022-08-20 19:43:57 -0700

    Committing previous files again

---
+++ client/src/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Weather({weather}) { // create a function that we can import in another file
+    return (
+        // className should match component name
+        <div className={'weather'}>
+            {weather.location}
+            <br/>
+            {weather.high}
+        </div>
+    )
+}
+
+// changed here
